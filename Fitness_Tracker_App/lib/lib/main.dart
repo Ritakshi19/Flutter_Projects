7e53e77b2f6b32accc3ad90c6cdce58b66899c87
Ritakshi Singh <109134641+Ritakshi19@users.noreply.github.com>
2025-06-16 18:58:46 +0530

    Delete Fitness_Tracker_App/lib directory

--- Fitness_Tracker_App/lib/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:fitness_tracker/Screens/home_screen.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MaterialApp(home: HomeScreen(), debugShowCheckedModeBanner: false));
-}
